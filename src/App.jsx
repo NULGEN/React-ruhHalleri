199bda84c302e63c260cf0a4485f868ade7c25d1
NULGEN <90576291+NULGEN@users.noreply.github.com>
2024-09-20 13:19:36 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import Sayac from './components/Sayac';
+import RuhHalleri from './components/RuhHalleri';
+
+function App() {
+  return (
+    <>
+      <RuhHalleri />
+    </>
+  );
+}
+
+export default App;
